67327fa991797ab986d2be3effee10e89162e42b
qt22 <qitian41322@gmail.com>
2022-01-16 17:48:00 -0500

    create like button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,31 @@
 <template>
-  <v-container fluid>
-    <v-row justify="center">
-      <v-col cols="12">
+  <v-container>
+    <v-row>
+      <v-col cols="12" v-for="image in nasaResponseData" :key="image.title">
         <v-card class="mx-auto" max-width="800">
           <v-img
-            :src="
-              nasaResponseData[0].hdurl
-                ? nasaResponseData[0].hdurl
-                : nasaResponseData[0].url
-            "
-            class="white--text align-end"
-            height="auto"
-            max-height="600px"
+            :src="image.hdurl ? image.hdurl : image.url"
+            height="600px"
             cover
           >
-            <v-card-title> card title </v-card-title>
           </v-img>
 
-          <v-card-subtitle class="pb-0"> card subtitle </v-card-subtitle>
+          <div class="d-flex justify-space-between align-center">
+            <v-card-title>{{ image.title }}</v-card-title>
+            <v-card-actions>
+              <v-btn :color="likeButtonColor" icon @click="toggleLikeButton()">
+                <v-icon>{{ likeIcon }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </div>
 
-          <v-card-text class="text--primary">
-            <div>some text</div>
-            <div>more more text</div>
+          <v-card-text class="text--primary pt-2">
+            <div>{{ image.explanation }}</div>
           </v-card-text>
 
-          <v-card-actions>
-            <v-btn icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
-          </v-card-actions>
+          <v-card-subtitle class="pt-0 pb-1">
+            {{ image.date }}
+          </v-card-subtitle>
         </v-card>
       </v-col>
     </v-row>
@@ -43,11 +40,13 @@ import axios from "axios";
 @Component
 export default class Home extends Vue {
   nasaResponseData: INasaApiData[] = [];
+  likeIcon: string = "mdi-heart-outline";
+  likeButtonColor = "grey";
 
   async created(): Promise<void> {
     axios
       .get(
-        `https://api.nasa.gov/planetary/apod?api_key=${process.env.VUE_APP_NASA_API_KEY}&count=5`
+        `https://api.nasa.gov/planetary/apod?api_key=${process.env.VUE_APP_NASA_API_KEY}&count=20`
       )
       .then((response) => {
         this.nasaResponseData = response.data;
@@ -56,6 +55,12 @@ export default class Home extends Vue {
       .catch((error) => {
         console.log(error);
       });
+  }
+
+  toggleLikeButton(): void {
+    this.likeButtonColor = this.likeButtonColor === "grey" ? "red" : "grey";
+    this.likeIcon =
+      this.likeIcon === "mdi-heart-outline" ? "mdi-heart" : "mdi-heart-outline";
   }
 }
 </script>
