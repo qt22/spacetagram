5d397c7a9a17f83fdcb44bf04e9f7b3b924c8fa8
qt22 <qitian41322@gmail.com>
2022-01-16 13:05:24 -0500

    send api request to nasa

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,10 +13,25 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 @Component
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  async created(): Promise<void> {
+    console.log(process.env.VUE_APP_NASA_API_KEY);
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=${process.env.VUE_APP_NASA_API_KEY}`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
